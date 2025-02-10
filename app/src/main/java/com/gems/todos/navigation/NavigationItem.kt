3e4b8b23a1dae466f2e8f09958db88ab79281730
kim_dayu15 <donoxojanazarova4060@gmail.com>
2025-02-10 12:10:57 +0500

    nav

--- app/src/main/java/com/gems/todos/navigation/NavigationItem.kt
+++ app/src/main/java/com/gems/todos/navigation/NavigationItem.kt
@@ -3,5 +3,4 @@ package com.gems.todos.navigation
 
 sealed class NavigationItem (val route: String){
     object TodoScreen : NavigationItem("todoScreen")
-    object ItemScreen : NavigationItem("itemScreen")
 }
